fcf17ce147546c2225e4f195b28f3c70ad8bf755
wuzhongli <wuzhongli@jusfoun.com>
2018-09-26 10:44:54 +0800

    from CRLF to LF

---
+++ src/main/java/com/wzl/datastructuresandalgorithmanalysis/codesegment/TestAutoBox.java
@@ -0,0 +1,9 @@
+package com.wzl.datastructuresandalgorithmanalysis.codesegment;
+
+public class TestAutoBox {
+
+    public static void main(String[] args) {
+        Integer a = 5;
+        int b = a;
+    }
+}
